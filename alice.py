5189747dc767a9f7c78d83d72b1ff1e30c1fc8a7
l0ki4 <alexey.domrachev98@gmail.com>
2019-12-13 08:11:44 +0300

    refactor code and start adding combinations

--- alice.py
+++ alice.py
@@ -4,49 +4,49 @@ import _thread
 import time
 import random
 
-from phe import paillier
+# from phe import paillier
 from common import *
 
-## Alice
+# Alice
 
-def Main():
-    ## Alice's server for initial reshuffling
-    ## Set up server to connect to Bob
+def main():
+    # Alice's server for initial reshuffling
+    # Set up server to connect to Bob
     server_alice = setServer('127.0.0.1', 5006, 2)
 
     print("Alice is up for the game.")
     print("Waiting for Bob to connect...\n")
 
-    ## Connect with Bob to start shuffling the deck
+    # Connect with Bob to start shuffling the deck
     connection_from_bob, address_bob = server_alice.accept()
     print("Connected to Bob.\n")
 
-    ## Set up initial game parameters
+    # Set up initial game parameters
     num_cards = 52
-    alice_key = random.randint(1,52)
+    alice_key = random.randint(1, 52)
 
-    ## Deck has numbers from 0 to 51
+    # Deck has numbers from 0 to 51
     deck = list(range(num_cards))
     print("A deck of ", num_cards, " cards received.")
 
-    ## Deck encryption by Alice
+    # Deck encryption by Alice
     for i in range(num_cards):
         deck[i] = encryptCard(deck[i], alice_key)
-    
-    ## Shuffle deck    
+
+    # Shuffle deck    
     random.shuffle(deck)
     print("Deck encrypted and shuffled.\n")
 
-    ## Send deck to Bob
+    # Send deck to Bob
     sendDeck(connection_from_bob, address_bob, deck)
     print("Deck sent to Bob.\n")
 
-    ## Receive encrypted and shuffled deck from Bob
+    # Receive encrypted and shuffled deck from Bob
     shuffled_deck_bob = connection_from_bob.recv(4096)
     shuffled_deck_bob = pickle.loads(shuffled_deck_bob)
     print("Deck received from Bob.")
 
-    ## Decrypt deck before individual encryption
+    # Decrypt deck before individual encryption
     for i in range(num_cards):
         shuffled_deck_bob[i] = decryptCard(shuffled_deck_bob[i], alice_key)
 
@@ -54,39 +54,39 @@ def Main():
 
     print("Getting individual keys...")
     alice_individual_keys = random.sample(range(1, 60), num_cards)
-    ## Encrypt each card with its individual key
+    # Encrypt each card with its individual key
     for i in range(num_cards):
         shuffled_deck_bob[i] = encryptCard(shuffled_deck_bob[i], alice_individual_keys[i])
     print("Deck encrypted by individual keys.\n")
 
-    ## Send deck to Bob for individual encryption
+    # Send deck to Bob for individual encryption
     sendDeck(connection_from_bob, address_bob, shuffled_deck_bob)
     print("Deck sent to Bob.\n")
 
-    ## Receive final deck from Bob
+    # Receive final deck from Bob
     shuffled_encrypted_cards = connection_from_bob.recv(4096)
     shuffled_encrypted_cards = pickle.loads(shuffled_encrypted_cards)
     print("Deck received from Bob.")
 
-    ## Distribute cards in half
+    # Distribute cards in half
     print("Distributing cards...\n")
 
     alice_cards = []
     alice_cards_keys1 = []
     bob_cards_keys2 = []
 
-    ## Alice gets first two cards
+    # Alice gets first two cards
     for i in range(2):
         alice_cards.append(shuffled_encrypted_cards[i])
         alice_cards_keys1.append(alice_individual_keys[i])
-    
-    ## Bob gets second two cards
-    for i in range(2,4):
+
+    # Bob gets second two cards
+    for i in range(2, 4):
         bob_cards_keys2.append(alice_individual_keys[i])
 
-    print("A hand of ", 2," cards received.\n")
-    
-    ## We need individual keys of both players for total decryption
+    print("A hand of ", 2, " cards received.\n")
+
+    # We need individual keys of both players for total decryption
     print("Sending individual keys of Bob's cards...\n")
     sendDeck(connection_from_bob, address_bob, bob_cards_keys2)
     print("Sent.")
@@ -96,20 +96,21 @@ def Main():
     alice_cards_keys2 = pickle.loads(alice_cards_keys2)
     print("Individual keys Received.\n")
 
-    ## Decrypt to see the hand you are dealt
+    # Decrypt to see the hand you are dealt
     print("Decrypting your cards...\n")
-    alice_cards_decrypted = [ 0 for i in range(2) ]
+    alice_cards_decrypted = [0 for i in range(2)]
     for i in range(2):
         alice_cards_decrypted[i] = decryptCard(decryptCard(alice_cards[i], alice_cards_keys1[i]), alice_cards_keys2[i])
 
     print("Your cards are : ")
-    print(alice_cards_decrypted)
+
+    print_cards_in_lst(alice_cards_decrypted)
     print("\n")
 
     print("We can start the game now..")
     print("Connecting to the servers...")
 
-    ## Connect to Server 1
+    # Connect to Server 1
     client_sock1 = connectToServer('127.0.0.1', 5004)
 
     packet = client_sock1.recv(4096)
@@ -117,24 +118,23 @@ def Main():
     rand_no = dicti["rand_no"]
     public_key = dicti["public_key"]
 
-    ## Connect to Server 2
+    # Connect to Server 2
     client_sock2 = connectToServer('127.0.0.1', 5005)
 
     print("Connected.\n")
     print("Alice! Throw your cards.\n")
     print("Enter the card index.")
 
-    ## Send you cards based on indexing
+    # Send you cards based on indexing
     for i in range(5):
-    	ind = int(input())
-    	encrypted_card = public_key.encrypt(alice_cards_decrypted[ind-1] * rand_no)
-    	data = pickle.dumps(encrypted_card, -1)
-    	client_sock2.sendall(data)
-    	result = client_sock1.recv(1024).decode('ascii')
-    	print(result)
+        ind = int(input())
+        encrypted_card = public_key.encrypt(alice_cards_decrypted[ind - 1] * rand_no)
+        data = pickle.dumps(encrypted_card, -1)
+        client_sock2.sendall(data)
+        result = client_sock1.recv(1024).decode('ascii')
+        print(result)
 
-    
-    ## Close all connections
+    # Close all connections
     client_sock2.close()
     client_sock1.close()
     connection_from_bob.close()
@@ -142,4 +142,4 @@ def Main():
 
 
 if __name__ == '__main__':
-    Main()
+    main()
--- common.py
+++ common.py
@@ -1,22 +1,31 @@
 import socket
 import pickle
+from collections import defaultdict
 
-## A commutative encryption function
+all_suits = "CSHD"
+values_dct = {2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9", 10: "10", 11: "J", 12: "Q", 13: "K",
+              14: "A"}
+hand_dct = {9: "straight-flush", 8: "four-of-a-kind", 7: "full-house", 6: "flush", 5: "straight", 4: "three-of-a-kind",
+            3: "two-pairs", 2: "one-pair", 1: "highest-card"}
+
+
+# A commutative encryption function
 def encryptCard(val, key):
     return val ^ key
 
 
-## Complementary decryption function
+# Complementary decryption function
 def decryptCard(val, key):
     return val ^ key
 
 
-## Send Deck to connection
+# Send Deck to connection
 def sendDeck(connection, address, deck):
     data = pickle.dumps(deck, -1)
     connection.sendall(data)
 
-## Set up server for messages
+
+# Set up server for messages
 def setServer(host, port, num_connections):
     server = socket.socket()
     server.bind((host, port))
@@ -24,14 +33,136 @@ def setServer(host, port, num_connections):
     return server
 
 
-## Connect to server for messages
+# Connect to server for messages
 def connectToServer(host, port):
     client_sock = socket.socket()
     client_sock.connect((host, port))
     return client_sock
 
 
-## Send Key value to other party in String format
+# Send Key value to other party in String format
 def sendKey(connection, key):
     connection.send(key.encode('ascii'))
 
+
+def get_card_by_num(card_num: int):
+    return values_dct[card_num // 4 + 2], all_suits[card_num % 4]
+
+
+def print_cards_in_lst(card_lst):
+    print([" ".join(get_card_by_num(card_num)) for card_num in card_lst])
+
+
+def check_straight_flush(hand):
+    if check_flush(hand) and check_straight(hand):
+        return True
+    else:
+        return False
+
+
+def get_hand_values(hand):
+    return [num // 4 + 2 for num in hand]
+
+
+def get_hand_suits(hand):
+    return [get_card_by_num(num)[1] for num in hand]
+
+
+def check_four_of_a_kind(hand):
+    values = get_hand_values(hand)
+    value_counts = defaultdict(lambda: 0)
+    for v in values:
+        value_counts[v] += 1
+    if 4 in value_counts.values():
+        return True
+    return False
+
+
+def check_full_house(hand):
+    values = get_hand_values(hand)
+    value_counts = defaultdict(lambda: 0)
+    for v in values:
+        value_counts[v] += 1
+    if 2 in value_counts.values() and 3 in value_counts.values():
+        return True
+    return False
+
+
+def check_flush(hand):
+    suits = get_hand_suits(hand)
+    if len(set(suits)) == 1:
+        return True
+    else:
+        return False
+
+
+# in progress
+def check_straight(hand):
+    values = get_hand_values(hand)
+    value_counts = defaultdict(lambda: 0)
+    for v in values:
+        value_counts[v] += 1
+    value_range = max(values) - min(values)
+    if value_range == 4:
+        return True
+    else:
+        # check straight with low Ace
+        if set(values) == set(["A", "2", "3", "4", "5"]):
+            return True
+        return False
+
+
+def check_three_of_a_kind(hand):
+    values = [i[0] for i in hand]
+    value_counts = defaultdict(lambda: 0)
+    for v in values:
+        value_counts[v] += 1
+    if set(value_counts.values()) == set([3, 1]):
+        return True
+    else:
+        return False
+
+
+def check_two_pairs(hand):
+    values = [i[0] for i in hand]
+    value_counts = defaultdict(lambda: 0)
+    for v in values:
+        value_counts[v] += 1
+    if sorted(value_counts.values()) == [1, 2, 2]:
+        return True
+    else:
+        return False
+
+
+def check_one_pairs(hand):
+    values = [i[0] for i in hand]
+    value_counts = defaultdict(lambda: 0)
+    for v in values:
+        value_counts[v] += 1
+    if 2 in value_counts.values():
+        return True
+    else:
+        return False
+
+
+def check_hand(hand):
+    # TODO: add royal flush
+    if check_straight_flush(hand):
+        return 9
+    if check_four_of_a_kind(hand):
+        return 8
+    if check_full_house(hand):
+        return 7
+    if check_flush(hand):
+        return 6
+    if check_straight(hand):
+        return 5
+    if check_three_of_a_kind(hand):
+        return 4
+    if check_two_pairs(hand):
+        return 3
+    if check_one_pairs(hand):
+        return 2
+    # TODO: implement same combinations compression
+    # always return highest card if no higher hands are found
+    return 1
