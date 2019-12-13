5c5a956971a831c7f3d90c0900f4b6ccef0d485a
l0ki4 <alexey.domrachev98@gmail.com>
2019-12-13 20:02:44 +0300

    add scoring and beautiful printing

--- common.py
+++ common.py
@@ -1,8 +1,14 @@
 import socket
 import pickle
-from collections import defaultdict
+import itertools
+from pocker import check_hand
 
-all_suits = "CSHD"
+all_suits = [u"\u2660".encode('utf-8'),  # spades
+             u"\u2764".encode('utf-8'),  # hearts
+             u"\u2666".encode('utf-8'),  # diamonds
+             u"\u2663".encode('utf-8')  # clubs
+             ]
+
 values_dct = {2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9", 10: "10", 11: "J", 12: "Q", 13: "K",
               14: "A"}
 hand_dct = {9: "straight-flush", 8: "four-of-a-kind", 7: "full-house", 6: "flush", 5: "straight", 4: "three-of-a-kind",
@@ -46,7 +52,7 @@ def send_key(connection, key):
 
 
 def get_card_by_num(card_num: int):
-    return values_dct[card_num // 4 + 2], all_suits[card_num % 4]
+    return values_dct[card_num // 4 + 2], (all_suits[card_num % 4]).decode("utf-8")
 
 
 def print_cards_in_lst(card_lst):
@@ -60,109 +66,3 @@ def get_hand_values(hand):
 def get_hand_suits(hand):
     return [get_card_by_num(num)[1] for num in hand]
 
-
-def check_straight_flush(hand):
-    if check_flush(hand) and check_straight(hand):
-        return True
-    else:
-        return False
-
-
-def check_four_of_a_kind(hand):
-    values = get_hand_values(hand)
-    value_counts = defaultdict(lambda: 0)
-    for v in values:
-        value_counts[v] += 1
-    if 4 in value_counts.values():
-        return True
-    return False
-
-
-def check_full_house(hand):
-    values = get_hand_values(hand)
-    value_counts = defaultdict(lambda: 0)
-    for v in values:
-        value_counts[v] += 1
-    if 2 in value_counts.values() and 3 in value_counts.values():
-        return True
-    return False
-
-
-def check_flush(hand):
-    suits = get_hand_suits(hand)
-    if len(set(suits)) == 1:
-        return True
-    else:
-        return False
-
-
-# in progress
-def check_straight(hand):
-    values = get_hand_values(hand)
-    value_counts = defaultdict(lambda: 0)
-    for v in values:
-        value_counts[v] += 1
-    value_range = max(values) - min(values)
-    if value_range == 4:
-        return True
-    else:
-        # check straight with low Ace
-        if set(values) == set(["A", "2", "3", "4", "5"]):
-            return True
-        return False
-
-
-def check_three_of_a_kind(hand):
-    values = [i[0] for i in hand]
-    value_counts = defaultdict(lambda: 0)
-    for v in values:
-        value_counts[v] += 1
-    if set(value_counts.values()) == set([3, 1]):
-        return True
-    else:
-        return False
-
-
-def check_two_pairs(hand):
-    values = [i[0] for i in hand]
-    value_counts = defaultdict(lambda: 0)
-    for v in values:
-        value_counts[v] += 1
-    if sorted(value_counts.values()) == [1, 2, 2]:
-        return True
-    else:
-        return False
-
-
-def check_one_pairs(hand):
-    values = [i[0] for i in hand]
-    value_counts = defaultdict(lambda: 0)
-    for v in values:
-        value_counts[v] += 1
-    if 2 in value_counts.values():
-        return True
-    else:
-        return False
-
-
-def check_hand(hand):
-    # TODO: add royal flush
-    if check_straight_flush(hand):
-        return 9
-    if check_four_of_a_kind(hand):
-        return 8
-    if check_full_house(hand):
-        return 7
-    if check_flush(hand):
-        return 6
-    if check_straight(hand):
-        return 5
-    if check_three_of_a_kind(hand):
-        return 4
-    if check_two_pairs(hand):
-        return 3
-    if check_one_pairs(hand):
-        return 2
-    # TODO: implement same combinations compression
-    # always return highest card if no higher hands are found
-    return 1
---
+++ pocker.py
@@ -0,0 +1,216 @@
+from collections import defaultdict
+import common
+import itertools
+
+
+def check_straight_flush(hand):
+    values = common.get_hand_values(hand)
+
+    if check_flush(hand)[0] and check_straight(hand)[0]:
+        return True, [max(values)], 0
+
+    return False, [], 0
+
+
+def check_four_of_a_kind(hand):
+    values = common.get_hand_values(hand)
+    value_counts = defaultdict(lambda: 0)
+    for v in values:
+        value_counts[v] += 1
+
+    if sorted(value_counts.values()) == (1, 4):
+        for key in value_counts:
+            if value_counts[key] == 4:
+                ans = key
+
+        return True, [ans], 1
+    return False, [], 1
+
+
+def check_full_house(hand):
+    values = common.get_hand_values(hand)
+    value_counts = defaultdict(lambda: 0)
+    for v in values:
+        value_counts[v] += 1
+
+    if sorted(value_counts.values()) == (2, 3):
+        ans = [0, 0]
+        for key in value_counts:
+            if value_counts[key] == 3:
+                ans[0] = key
+            elif value_counts[key] == 2:
+                ans[1] = key
+
+        return True, ans, 0
+    return False, [], 0
+
+
+def check_flush(hand):
+    suits = common.get_hand_suits(hand)
+    if len(set(suits)) == 1:
+        return True, sorted(common.get_hand_values(hand), reverse=True), 0
+
+    return False, [], 0
+
+
+def check_straight(hand):
+    values = common.get_hand_values(hand)
+    value_counts = defaultdict(lambda: 0)
+    for v in values:
+        value_counts[v] += 1
+
+    value_range = max(values) - min(values)
+    if value_range == 4:
+        return True, [max(values)], 0
+
+    # check straight with low Ace
+    if set(values) == {14, 2, 3, 4, 5}:
+        return True, [5], 0
+
+    return False, [], 0
+
+
+def check_three_of_a_kind(hand):
+    values = common.get_hand_values(hand)
+    value_counts = defaultdict(lambda: 0)
+    for v in values:
+        value_counts[v] += 1
+
+    if 3 in value_counts.values():
+        for key in value_counts:
+            if value_counts[key] == 3:
+                ans = key
+
+        return True, [ans], 2
+
+    return False, [], 2
+
+
+def check_two_pairs(hand):
+    values = common.get_hand_values(hand)
+    value_counts = defaultdict(lambda: 0)
+    for v in values:
+        value_counts[v] += 1
+
+    if set(value_counts.values()) == {1, 2, 2}:
+        pair1 = 0
+        pair2 = 0
+
+        for key in value_counts:
+            if value_counts[key] == 2:
+                if key != pair1:
+                    pair1 = key
+                else:
+                    pair2 = key
+
+        return True, sorted([pair1, pair2], reverse=True), 1
+    return False, [], 1
+
+
+def check_one_pairs(hand):
+    values = common.get_hand_values(hand)
+    value_counts = defaultdict(lambda: 0)
+    for v in values:
+        value_counts[v] += 1
+
+    if 2 in value_counts.values():
+        for key in value_counts:
+            if value_counts[key] == 2:
+                ans = key
+
+        return True, [ans], 3
+    return False, [], 3
+
+
+def check_hand(hand):
+    flag, ans, kickers_amount = check_straight_flush(hand)
+    if flag:
+        return 9, ans, kickers_amount
+
+    flag, ans, kickers_amount = check_four_of_a_kind(hand)
+    if flag:
+        return 8, ans, kickers_amount
+
+    flag, ans, kickers_amount = check_full_house(hand)
+    if flag:
+        return 7, ans, kickers_amount
+
+    flag, ans, kickers_amount = check_flush(hand)
+    if flag:
+        return 6, ans, kickers_amount
+
+    flag, ans, kickers_amount = check_straight(hand)
+    if flag:
+        return 5, ans, kickers_amount
+
+    flag, ans, kickers_amount = check_three_of_a_kind(hand)
+    if flag:
+        return 4, ans, kickers_amount
+
+    flag, ans, kickers_amount = check_two_pairs(hand)
+    if flag:
+        return 3, ans, kickers_amount
+
+    flag, ans, kickers_amount = check_one_pairs(hand)
+    if flag:
+        return 2, ans, kickers_amount
+
+    return 1, [], kickers_amount
+
+
+
+def get_hand_score(hand, table_cards):
+    combinations = itertools.combinations(hand + table_cards, 5)
+    score = 0
+    ans = []
+    kickers_amount = 0
+
+    for combination in combinations:
+        curr_score, ans_curr, kickers_amount_curr = check_hand(combination)
+        if curr_score > score:
+            score = curr_score
+            ans = ans_curr
+            kickers_amount = kickers_amount_curr
+
+    kickers = []
+    for card in hand + table_cards:
+        if card not in ans:
+            kickers.append(card)
+
+    kickers = sorted(kickers, reverse=True)[:kickers_amount]
+    return score, ans, kickers
+
+
+def choose_winner(left_hand, right_hand, table_cards):
+    left_score, left_ans, left_kickers = get_hand_score(left_hand, table_cards)
+    right_score, right_ans, right_kickers = get_hand_score(right_hand, table_cards)
+
+    if left_score > right_score:
+        return 1, 0
+    elif left_score < right_score:
+        return 0, 1
+
+    for left_card, right_card in zip(left_ans, right_ans):
+        if left_card > right_card:
+            return 1, 0
+        elif left_card < right_card:
+            return 0, 1
+
+    for left_kicker, right_kicker in zip(left_kickers, right_kickers):
+        if left_kicker > right_kicker:
+            return 1, 0
+        elif left_kicker < right_kicker:
+            return 0, 1
+
+    return 0, 0
+
+
+if __name__ == '__main__':
+    left_hand = [5, 6]
+    right_hand = [10, 11]
+    table = [1, 34, 8, 4, 3]
+    common.print_cards_in_lst(table)
+    common.print_cards_in_lst(left_hand)
+    common.print_cards_in_lst(right_hand)
+
+    print(choose_winner(left_hand, right_hand, table))
